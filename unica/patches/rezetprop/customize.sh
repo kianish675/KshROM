fe06528e3696343aeab459098bf9d8ea6b656e20
Kianish (@ksh675) <221608370+kianish675@users.noreply.github.com>
2025-11-09 07:57:24 +0000

    unica: patches: Add back patches
    Signed-off-by: Kianish (@ksh675) <221608370+kianish675@users.noreply.github.com>

--- unica/patches/rezetprop/customize.sh
+++ unica/patches/rezetprop/customize.sh
@@ -1,46 +1,29 @@
-BL_SPOOF="$(GET_PROP "ro.build.version.incremental")"
-MODEL_SPOOF="$(GET_PROP "ro.product.system.model")"
+echo "- Setting up rezetprop.."
 
-echo "Rezetprop Setup"
-echo "Spoofed BL: "$BL_SPOOF
-echo "Spoofed Model: "$MODEL_SPOOF
-if [[ -v SOURCE_PRODUCT_CODE ]]; then
-    echo "Spoofed Product Code: "$SOURCE_PRODUCT_CODE
-else
-    echo "No referenced product code value was found in source"
-fi
+SECURITY_PATCH="$(GET_PROP "ro.build.version.security_patch")"
 
 {
     echo "on property:service.bootanim.exit=1"
-    echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -p -d persist.sys.pixelprops.games"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.flash.locked 1"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.vbmeta.device_state locked"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.verifiedbootstate green"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.veritymode enforcing"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.warranty_bit 0"
-    echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.bootloader "$BL_SPOOF
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n sys.oem_unlock_allowed 0"
-    echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n gsm.version.baseband "$BL_SPOOF","$BL_SPOOF
-    if [[ -v SOURCE_PRODUCT_CODE ]]; then
-        echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ril.product_code "$SOURCE_PRODUCT_CODE
-    fi
-    echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ril.sw_ver "$BL_SPOOF
-    echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.em.model "$MODEL_SPOOF
+    echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.vendor.build.security_patch "$SECURITY_PATCH
     echo ""
     echo "on property:sys.unica.vbmeta.digest=*"
     echo '    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.vbmeta.digest ${sys.unica.vbmeta.digest}'
     echo ""
 } >> "$WORK_DIR/system/system/etc/init/hw/init.rc"
 
+echo "- Succesfully finished rezetprop setup."
+
 sed -i 's/${ro.boot.warranty_bit}/0/g' "$WORK_DIR/system/system/etc/init/init.rilcommon.rc"
 
-echo "Setting up SEPolicy"
+echo "Setting up SEPolicy.."
 LINES="$(sed -n "/^(allow init init_exec\b/=" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil")"
 for l in $LINES; do
     sed -i "${l} s/)))/ execute_no_trans)))/" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
 done
-
-echo "Patching complete!"
-echo "Cleaning up..."
-BL_SPOOF=
-MODEL_SPOOF=
+echo "- SEPolicy is set up."
