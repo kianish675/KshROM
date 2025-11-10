5cf27c36b0780da532ee905bbc584ab7e299460e
Kianish (@ksh675) <221608370+kianish675@users.noreply.github.com>
2025-11-10 18:56:40 +0000

    unica: change fw base to S21 FE
    * may be temporary
    * also rolled back legacy_stack
    
    Signed-off-by: Kianish (@ksh675) <221608370+kianish675@users.noreply.github.com>

--- unica/patches/legacy_stack/customize.sh
+++ unica/patches/legacy_stack/customize.sh
@@ -1,36 +1,31 @@
 {
 mkdir $WORK_DIR/system/system/system_ext/apex
 } || {
-echo "/system/system_ext/apex: Already a folder"
+echo "Apex folder already exists"
 }
 
-echo "- Adding S23 FE (r11sxxx) lib/ blobs.."
-ADD_TO_WORK_DIR "r11sxxx" "system" "system/lib" 0 0 644
+echo "Applying Legacy stack from S21 FE (r9qxxx)"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/apex/com.android.runtime.apex" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/apex/com.android.i18n.apex" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/bin/bootstrap" 0 2000 751 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/bin/insthk" 0 2000 755 "u:object_r:insthk_exec:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/bin/linker" 0 2000 755 "u:object_r:system_linker_exec:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/bin/linker_asan" 0 2000 755 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/lib" 0 0 755 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$SOURCE_EXTRA_FIRMWARES" "system" "system/system_ext/apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
 
+# THIS IS A VERY DIRTY HACK TO APPLY SYSTEM_EXT 32-BIT STACK LIBRARIES TO OUR FIRMWARE
+# Reason is the build system crumbles while attempting to add the files in
+echo "Dirty adding system_ext from S21 FE (r9qxxx)"
 
-echo "- Adding 32-bit support from S23 FE (r11sxxx) & S21 FE (r9qxxx)"
-ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.runtime.apex" 0 0 644 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.i18n.apex" 0 0 644 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "r11sxxx" "system" "system/bin/bootstrap" 0 2000 751 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/bin/insthk" 0 2000 755 "u:object_r:insthk_exec:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/lib" 0 0 755 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/system_ext/apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
+MODEL=$(echo -n "$SOURCE_EXTRA_FIRMWARES" | cut -d "/" -f 1)
+REGION=$(echo -n "$SOURCE_EXTRA_FIRMWARES" | cut -d "/" -f 2)
 
-echo "- Setting up symlinks.. "
-ln -sf "/apex/com.android.runtime/bin/linker" "$WORK_DIR/system/system/bin/linker"
-ln -sf "/apex/com.android.runtime/bin/linker" "$WORK_DIR/system/system/bin/linker_asan"
-SET_METADATA "system" "system/bin/linker" 0 0 755 "u:object_r:system_file:s0"
-SET_METADATA "system" "system/bin/linker_asan" 0 0 755 "u:object_r:system_file:s0"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/system_ext/lib" "$WORK_DIR/system/system/system_ext"
+cat "$FW_DIR/${MODEL}_${REGION}/fs_config-system" | grep -F "system/system_ext/lib" >> "$WORK_DIR/configs/fs_config-system"
+cat "$FW_DIR/${MODEL}_${REGION}/file_context-system" | grep -F "system/system_ext/lib" >> "$WORK_DIR/configs/file_context-system"
+echo "system/system_ext/lib 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+echo "/system/system_ext/lib u:object_r:system_lib_file:s0" >> "$WORK_DIR/configs/file_context-system"
 
-ln -sf "/apex/com.android.runtime/lib/bionic/libc.so" "$WORK_DIR/system/system/lib/libc.so"
-ln -sf "/apex/com.android.runtime/lib/bionic/libdl.so" "$WORK_DIR/system/system/lib/libdl.so"
-ln -sf "/apex/com.android.runtime/lib/bionic/libdl_android.so" "$WORK_DIR/system/system/lib/libdl_android.so"
-ln -sf "/apex/com.android.runtime/lib/bionic/libm.so" "$WORK_DIR/system/system/lib/libm.so"
-SET_METADATA "system" "system/lib/libc.so" 0 0 644 "u:object_r:system_lib_file:s0"
-SET_METADATA "system" "system/lib/libdl.so" 0 0 644 "u:object_r:system_lib_file:s0"
-SET_METADATA "system" "system/lib/libdl_android.so" 0 0 644 "u:object_r:system_lib_file:s0"
-SET_METADATA "system" "system/lib/libm.so" 0 0 644 "u:object_r:system_lib_file:s0"
-echo - "Done setting up symlinks!"
-
-echo "- Added 32-bit support."
+echo "Legacy stack was applied successfully!"
